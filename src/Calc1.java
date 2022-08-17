d8e29668b744fabb16686ad4e2393a695a82063f
Viktoria Lokatyrieva <mailian.vikaM@gmail.com>
2022-08-17 19:16:05 +0300

    add changes

--- src/Calc1.java
+++ src/Calc1.java
@@ -1,7 +1,4 @@
 public class Calc1 {
-    int Param1; // 10
-    int Param2; // 5
-    static char operation; // + - / *
 
     static void Calc1(int number1, int number2, char operation){
         if (operation == '+') {
